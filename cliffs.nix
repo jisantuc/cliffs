f0cabfd6c71777fb5e34d28da4563af6186b1330
James Santucci <james.santucci@gmail.com>
2021-11-10 11:43:32 -0500

    Make a real nix derivation instead of just having a shell (#11)
    * Bundle up cabal2nix package
    
    See https://haskell4nix.readthedocs.io/nixpkgs-users-guide.html#
    
    * Remove haskell.nix
    
    * Remove CI steps that I _think_ are no longer needed
    
    * Test caching
    
    * Well who cares then maybe I don't need it?
    
    * add cd
    
    * debug bin location
    
    * why am i conditionally checking out repo
    
    * back to tags only
    
    * Explicitly use cachix
    
    * Use cachix-action instead
    
    * Deliberately fail a test to see if build is mad
    
    * Skip test step since they run in build
    
    https://github.com/jisantuc/cliffs/runs/4148138615?check_suite_focus=true
    
    * Remove pr trigger from cd
    
    * Add cachix back to cd job
    
    * Update changelog

---
+++ cliffs.nix
@@ -0,0 +1,70 @@
+{ mkDerivation
+, base
+, brick
+, cmark-gfm
+, command
+, containers
+, directory
+, filepath
+, hpack
+, hspec
+, hspec-discover
+, lib
+, microlens
+, text
+, vector
+, vty
+}:
+mkDerivation {
+  pname = "cliffs";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    base
+    brick
+    cmark-gfm
+    command
+    containers
+    directory
+    filepath
+    microlens
+    text
+    vector
+    vty
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    base
+    brick
+    cmark-gfm
+    command
+    containers
+    directory
+    filepath
+    microlens
+    text
+    vector
+    vty
+  ];
+  testHaskellDepends = [
+    base
+    brick
+    cmark-gfm
+    command
+    containers
+    directory
+    filepath
+    hspec
+    hspec-discover
+    microlens
+    text
+    vector
+    vty
+  ];
+  testToolDepends = [ hspec-discover ];
+  prePatch = "hpack";
+  homepage = "https://github.com/jisantuc/cliffs#readme";
+  license = lib.licenses.mit;
+}
